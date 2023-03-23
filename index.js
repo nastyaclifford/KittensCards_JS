efd3957d978cea623c36e1f5cd1309cd048d3811
nastyaclifford <madchen95@mail.ru>
2023-03-23 10:12:10 -0600

    added Task 1 and 2

---
+++ index.js
@@ -0,0 +1,13 @@
+//Задание 1
+function showMessage(text) {
+  console.log(text);
+}
+showMessage(`Я учу JavaScript!`);
+
+//Задание 2
+function button_right() {
+  document.getElementById("image").src = "assets/comingKitten.jpg";
+}
+function button_left() {
+  document.getElementById("image").src = "assets/kitten.jpg";
+}
